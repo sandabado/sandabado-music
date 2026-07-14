0905f5ed7fbb689867e3c5b39a47cdf746cb5c80
sandabado <jesse.gawlik@proton.me>
2026-07-13 17:31:12 -0700

    Add fluid Sandabado S mark

---
+++ components/sandabado/sandabado-mark.tsx
@@ -0,0 +1,5 @@
+export function SandabadoMark() {
+  return <div className="hero-brand-mark mx-auto" aria-hidden="true">
+    <img src="/images/brand/sandabado-mark.svg" alt="" width="240" height="300" draggable={false}/>
+  </div>
+}
